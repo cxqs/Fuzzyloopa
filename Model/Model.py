db03ff1519eca9319b6c6b42957164d2f7c774d5
A6a7H <dikrivenkov@edu.hse.ru>
2019-03-22 11:35:13 +0300

    Modelv3

--- Model/Model.py
+++ Model/Model.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import TimeSeriesSplit
 
 class Fuzzyloopa():
 
-    def __init__(self, n_inputs, n_rules):
+    def __init__(self, n_inputs, n_rules, learning_rate = 1e-2):
         self.__numI = n_inputs
         self.__numR = n_rules
         self.inputs = tf.placeholder(tf.float32, shape=(None, self.__numI))
@@ -17,17 +17,28 @@ class Fuzzyloopa():
         self.rules = tf.reduce_prod(
             tf.reshape(tf.exp(-0.5 * tf.square(tf.subtract(tf.tile(self.inputs, (1, n_rules)), self.ai) / (self.ci))),
                        (-1, n_rules, n_inputs)), axis=2)
-        self.rules = self.rules / tf.reduce_sum(self.rules)
-        __b = tf.Variable(np.random.randn(), name='b')
-        __w = tf.Variable(np.random.randn(), name='w')
-        self.y_pred = tf.reduce_sum(tf.add(tf.multiply(self.rules, __w), __b))
+        # self.rules = self.rules / tf.reduce_sum(self.rules)
+        # __b = tf.Variable(np.random.randn(), name='b')
+        # __w = tf.Variable(np.random.randn(), name='w')
+        # self.y_pred = tf.reduce_sum(tf.add(tf.multiply(self.rules, __w), __b), axis=1)
 
         y = tf.get_variable('y', [1, n_rules], initializer=tf.random_normal_initializer(0,1))
-        # self.rules = tf.exp(-tf.square((tf.tile(self.inputs, (1, n_rules)) - self.ai) / self.ci))
-        # self.rules = tf.reshape(self.rules,(-1, n_rules, n_inputs))
-        self.loss = tf.losses.huber_loss()
-        self.optimize = tf.train.AdamOptimizer(learning_rate=0.01).minimize(self.loss)
+        num = tf.reduce_sum(tf.multiply(self.rules, y), axis=1)
+        den = tf.clip_by_value(tf.reduce_sum(self.rules, axis=1), 1e-12, 1e12)
+        self.out = tf.divide(num, den)
 
+        self.loss = tf.losses.huber_loss(self.targets, self.out)
+        self.optimize = tf.train.AdamOptimizer(learning_rate=0.01).minimize(self.loss)
+        self.init_variables = tf.global_variables_initializer()
+
+
+    def make_prediction(self, sess, x):
+        return sess.run(self.out, feed_dict={self.inputs:x})
+
+
+    def train(self, sess, x , targets):
+        out, l, _ = sess.run([self.out, self.loss, self.optimize], feed_dict={self.inputs: x, self.targets: targets})
+        return l, out
 # rules = tf.constant([[1, 1, 1, 1], [1,1,1,1], [1,1,1,1]])
 # input = tf.constant([1,2,1,1],[1,1,1,1])
 # n_rules = 2
@@ -36,9 +47,9 @@ class Fuzzyloopa():
 #             tf.reshape(tf.exp(-0.5 * tf.square(tf.subtract(tf.tile(rules, (1, n_rules)), input)) / tf.square(input)),
 #                        (-1, n_rules, 2)), axis=2)
 # ai = tf.get_variable('ai', [3 * 5], initializer=tf.random_normal_initializer(0,1))
-
-with tf.Session() as sess:
-    a1 = tf.get_variable('ai', [3 * 5], initializer=tf.random_normal_initializer(0,1))
-    a1.initializer.run()
-    print(a1.eval())
+if __name__ == "__main__":
+    with tf.Session() as sess:
+        a1 = tf.get_variable('ai', [3 * 5], initializer=tf.random_normal_initializer(0,1))
+        a1.initializer.run()
+        print(a1.eval())
 
