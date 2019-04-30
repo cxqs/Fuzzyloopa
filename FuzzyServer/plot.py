b52e958c820bd4c3687fb7fde61909112df54625
A6a7H <dikrivenkov@edu.hse.ru>
2019-04-30 14:30:29 +0300

    solve ploting

--- FuzzyServer/plot.py
+++ FuzzyServer/plot.py
@@ -1,26 +1,41 @@
 from flask import Flask, Markup, render_template, request, redirect, url_for
+from Preprocessing.Preprocessing import Prepocessing as prep
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
-labels = [
-    'JAN', 'FEB', 'MAR', 'APR',
-    'MAY', 'JUN', 'JUL', 'AUG',
-    'SEP', 'OCT', 'NOV', 'DEC'
-]
+D = 15  # number of regressors
 
-values = [
-    967.67, 1190.89, 1079.75, 1349.19,
-    2328.91, 2504.28, 2873.83, 4764.87,
-    4349.29, 6458.30, 9907, 16297
-]
+series= prep('../TimeSeries/Algn.csv', D, 1, "Adj Close")
+series.print_info()
+series.creat_features()
+series.normalize_features()
+series.creat_target()
 
+# data_new = np.array(series.features)[:50]
+lbl = []
+for i in series.targets[:1000]:
+    lbl.append(i[0])
+lbls_new = lbl
+
+# labels = [
+#     'JAN', 'FEB', 'MAR', 'APR',
+#     'MAY', 'JUN', 'JUL', 'AUG',
+#     'SEP', 'OCT', 'NOV', 'DEC'
+# ]
+#
+
+labels = [i for i in range(len(lbls_new[:1000]))]
+values = lbls_new[:1000]
 
 @app.route('/')
 def line(line = 'Bitcoin Monthly Price in USD'):
     line_labels = labels
     line_values = values
-    return render_template('base.html', title=line, max=17000, labels=line_labels,
-                           values=line_values)
+    # return render_template('base.html', title=line, max=17000, labels=line_labels,
+    #                        values=line_values)
+    return render_template('base.html', title='Ploting...', max=17000, labels=line_labels, values=line_values)
 
 
 @app.route('/predict/', methods=['POST'])
--- Model/test.py
+++ Model/test.py
@@ -33,7 +33,7 @@ def mackey(n_iters):
 # chkData = data[lbls.size - round(lbls.size * 0.3):, :]
 # chkLbls = lbls[lbls.size - round(lbls.size * 0.3):]
 
-D = 10  # number of regressors
+D = 15  # number of regressors
 T = 1  # delay
 # N = 1000  # Number of points to generate
 # frame = pd.read_csv('TimeSeries/zuerich.csv')
@@ -42,14 +42,14 @@ T = 1  # delay
 # frame = pd.read_csv('TimeSeries/Algn.csv')
 # series = frame['Adj Close'].values[:1000]
 
-series= prep('TimeSeries/Algn.csv', D, 1, "Adj Close")
+series= prep('TimeSeries/zuerich.csv', D, 1, "Zuerich")
 series.print_info()
 series.creat_features()
 series.normalize_features()
 series.creat_target()
 
-data_new = np.array(series.features)[500:2000]
-lbls_new = np.array(series.targets)[500:2000]
+data_new = np.array(series.features)[:2800]
+lbls_new = np.array(series.targets)[:2800]
 
 trnData_new = data_new[:lbls_new.size - round(len(lbls_new) * 0.3), :]
 trnLbls_new = lbls_new[:lbls_new.size - round(lbls_new.size * 0.3)]
@@ -57,14 +57,16 @@ chkData_new = data_new[lbls_new.size - round(lbls_new.size * 0.3):, :]
 chkLbls_new = lbls_new[lbls_new.size - round(lbls_new.size * 0.3):]
 
 
-m = 25  # number of rules
-# m = 10
-alpha = 0.01  # learning rate
+# m = 22  # number of rules best for 1
+m = 22
+alpha = 0.001  # learning rate
+# batch_size = 700 best for
+batch_size = 700
 
 # cfis = Fuzzyloopa(n_inputs=D, n_rules=m, learning_rate=alpha)
 cfis = CFuzzyloopa(n_inputs=D, n_rules=m, n_output=1, learning_rate=alpha)
 # Training
-num_epochs = 5000
+num_epochs = 20000
 # Initialize session to make computations on the Tensorflow graph
 with tf.Session() as sess:
     sess.run(cfis.init_variables)
@@ -73,22 +75,25 @@ with tf.Session() as sess:
     before = 0
     ai_ = []
     ci_ = []
+    trn_loss, trn_pred = None, None
     for epoch in range(num_epochs):
-        trn_loss, trn_pred, ai_, ci_ = cfis.train(sess, trnData_new, trnLbls_new)
-        # ctrn_loss, ctrn_pred, cai_, cci_ = cfis.train(sess, trnData_new, trnLbls_new)
+        for i in range(0,len(trnData_new),batch_size):
+            trn_loss, trn_pred, ai_, ci_ = cfis.train(sess, trnData_new[i:i+batch_size], trnLbls_new[i:i+batch_size])
+        trn_pred, trn_loss = cfis.make_prediction(sess, trnData_new, trnLbls_new)
         if epoch % 10 == 0:
             print("Train cost after epoch %i: \nhuber: %f" % (epoch, trn_loss))
             mad = cfis.MAD(trn_pred, trnLbls_new)
             mape = cfis.MAPE(trn_pred, trnLbls_new)
             smape = cfis.SMAPE(trn_pred, trnLbls_new)
             print('mape: {}\nmad: {}\nsmape: {}\n'.format(mape, mad, smape))
+            # ctrn_loss, ctrn_pred, cai_, cci_ = cfis.train(sess, trnData_new, trnLbls_new)
         if epoch == num_epochs - 1:
             # cfis.plot_rules(sess, 2)
             val_pred, val_loss = cfis.make_prediction(sess, chkData_new, chkLbls_new)
             val_pred_ex, val_loss_ex = cfis.make_prediction(sess, chkData_new[0:3], chkLbls_new[0:3])
             # pred = np.vstack((np.expand_dims(trn_pred, 1), np.expand_dims(val_pred, 1)))
             print(val_loss)
-            # plt.plot([x for x in range(0, lbls_new.size)], lbls_new, color='blue')
-            # plt.plot([x for x in range(lbls_new.size - round(len(lbls_new) * 0.3), lbls_new.size)], val_pred, color='yellow')
-            # plt.plot([x for x in range(0,lbls_new.size - round(len(lbls_new) * 0.3))], trn_pred, color='green')
+            plt.plot([x for x in range(0, lbls_new.size)], lbls_new, color='blue')
+            plt.plot([x for x in range(lbls_new.size - round(len(lbls_new) * 0.3), lbls_new.size)], val_pred, color='yellow')
+            plt.plot([x for x in range(0,lbls_new.size - round(len(lbls_new) * 0.3))], trn_pred, color='green')
     plt.show()
