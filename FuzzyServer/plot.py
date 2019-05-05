e376da8180553740f736d55f4519dc7f302d353c
A6a7H <dikrivenkov@edu.hse.ru>
2019-05-05 16:18:19 +0300

    add_data

--- FuzzyServer/plot.py
+++ FuzzyServer/plot.py
@@ -10,6 +10,7 @@ from Preprocessing.Preprocessing import Prepocessing as prep
 app = Flask(__name__)
 
 D = 15
+T = 1
 ALPHA = 0.01  # learning rate
 m = 22
 ai_algn = 0
@@ -25,7 +26,7 @@ with open('../Weights/Algn_22_1.txt') as json_file:
 
 @app.route('/')
 def line(line = 'Bitcoin Monthly Price in USD'):
-    series = prep('../TimeSeries/Algn.csv', D, 1, "Adj Close")
+    series = prep('../TimeSeries/Algn.csv', D, T, "Adj Close")
     series.creat_features()
     series.normalize_features()
     series.creat_target()
@@ -33,7 +34,7 @@ def line(line = 'Bitcoin Monthly Price in USD'):
     data = np.array(series.features)[:2800]
     lbls = np.array(series.targets)[:2800]
 
-    cfis = CFuzzyloopa(n_inputs=D, n_rules=m, n_output=1, learning_rate=ALPHA, ai=ai_algn, ci=ci_algn, y=y_algn)
+    cfis = CFuzzyloopa(n_inputs=D, n_rules=m, n_output=T, learning_rate=ALPHA, ai=ai_algn, ci=ci_algn, y=y_algn)
 
     with tf.Session() as sess:
         sess.run(cfis.init_variables)
@@ -59,7 +60,7 @@ def line(line = 'Bitcoin Monthly Price in USD'):
 
 @app.route('/predict_align/', methods=['POST'])
 def do_align():
-    series = prep('../TimeSeries/Algn.csv', D, 1, "Adj Close")
+    series = prep('../TimeSeries/Algn.csv', D, T, "Adj Close")
     series.creat_features()
     series.normalize_features()
     series.creat_target()
@@ -67,7 +68,7 @@ def do_align():
     data = np.array(series.features)[:2800]
     lbls = np.array(series.targets)[:2800]
 
-    cfis = CFuzzyloopa(n_inputs=D, n_rules=m, n_output=1, learning_rate=ALPHA, ai=ai_algn, ci=ci_algn, y=y_algn)
+    cfis = CFuzzyloopa(n_inputs=D, n_rules=m, n_output=T, learning_rate=ALPHA, ai=ai_algn, ci=ci_algn, y=y_algn)
 
     with tf.Session() as sess:
         sess.run(cfis.init_variables)
@@ -90,25 +91,45 @@ def do_align():
 
     return render_template('base.html', labels=line_labels, values=line_values, predic=prediction)
 
-@app.route('/predict_McCensi/', methods=['POST'])
+@app.route('/predict_Zuerich/', methods=['POST'])
 def doit():
-    series = prep('../TimeSeries/daily.csv', D, 1, "Daily")
-    series.print_info()
+
+    with open('../Weights/Zuerich_22_1_15.txt') as json_file:
+        data = json.load(json_file)
+        ai_Zuerich = data['ai']
+        ci_Zuerich = data['ci']
+        y_Zuerich = data['y']
+
+    series = prep('../TimeSeries/zuerich.csv', D, T, "Zuerich")
     series.creat_features()
     series.normalize_features()
     series.creat_target()
 
-    lbl = []
-    for i in series.targets[:1000]:
-        lbl.append(i[0])
-    lbls_new = lbl
+    data = np.array(series.features)[:2800]
+    lbls = np.array(series.targets)[:2800]
 
-    labels = [i for i in range(len(lbls_new[:1000]))]
-    values = lbls_new[:1000]
+    cfis = CFuzzyloopa(n_inputs=D, n_rules=m, n_output=T, learning_rate=ALPHA, ai=ai_Zuerich, ci=ci_Zuerich, y=y_Zuerich)
 
-    line_labels = labels
-    line_values = values
+    with tf.Session() as sess:
+        sess.run(cfis.init_variables)
 
-    return render_template('base.html', labels=line_labels, values=line_values)
+        chkData_new = data[lbls.size - round(lbls.size * 0.3):, :]
+        chkLbls_new = lbls[lbls.size - round(lbls.size * 0.3):]
+
+        all_data = []
+        for ch in lbls:
+            all_data.append(ch[0])
+
+        val_pred, val_loss = cfis.make_prediction(sess, chkData_new, chkLbls_new)
+        prediction = [-1 for i in range(len(all_data) - len(val_pred))]
+        for ch in val_pred:
+            prediction.append(ch[0])
+
+        labels = [i for i in range(2800)]
+        line_labels = labels
+        line_values = all_data
+
+    return render_template('base.html', labels=line_labels, values=line_values, predic=prediction)
+
 if __name__ == '__main__':
     app.run()
--- Model/test.py
+++ Model/test.py
@@ -6,42 +6,10 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 
-
-def mackey(n_iters):
-    x = np.zeros((n_iters,))
-    x[0:30] = 0.23 * np.ones((30,))
-    t_s = 30
-    for i in range(30, n_iters - 1):
-        a = x[i]
-        b = x[i - t_s]
-        y = ((0.2 * b) / (1 + b ** 10)) + 0.9 * a
-        x[i + 1] = y
-    return x
-
-
-# data = np.zeros((N - T - (D - 1) * T, D))
-# lbls = np.zeros((N - T - (D - 1) * T,))
-
-
-
-# for t in range((D - 1) * T, N - T):
-#     data[t - (D - 1) * T, :] = [mg_series[t - 3 * T], mg_series[t - 2 * T], mg_series[t - T], mg_series[t]]
-#     lbls[t - (D - 1) * T] = mg_series[t + T]
-# trnData = data[:lbls.size - round(len(lbls) * 0.3), :]
-# trnLbls = lbls[:lbls.size - round(lbls.size * 0.3)]
-# chkData = data[lbls.size - round(lbls.size * 0.3):, :]
-# chkLbls = lbls[lbls.size - round(lbls.size * 0.3):]
-
 D = 15  # number of regressors
 T = 1  # delay
-# N = 1000  # Number of points to generate
-# frame = pd.read_csv('TimeSeries/zuerich.csv')
-# mg_series = mackey(N)[499:]  # Use last 1500 points
-# mg_series = frame["Zuerich"].values
-# frame = pd.read_csv('TimeSeries/Algn.csv')
-# series = frame['Adj Close'].values[:1000]
 
-series= prep('TimeSeries/Algn.csv', D, 1, 'Adj Close')
+series= prep('TimeSeries/zuerich.csv', D, T, 'Zuerich')
 series.print_info()
 series.creat_features()
 series.normalize_features()
@@ -50,10 +18,10 @@ series.creat_target()
 data_new = np.array(series.features)[:2800]
 lbls_new = np.array(series.targets)[:2800]
 
-trnData_new = data_new[:lbls_new.size - round(len(lbls_new) * 0.3), :]
-trnLbls_new = lbls_new[:lbls_new.size - round(lbls_new.size * 0.3)]
-chkData_new = data_new[lbls_new.size - round(lbls_new.size * 0.3):, :]
-chkLbls_new = lbls_new[lbls_new.size - round(lbls_new.size * 0.3):]
+trnData_new = data_new[:len(lbls_new) - round(len(lbls_new) * 0.3), :]
+trnLbls_new = lbls_new[:len(lbls_new) - round(len(lbls_new) * 0.3)]
+chkData_new = data_new[len(lbls_new) - round(len(lbls_new) * 0.3):, :]
+chkLbls_new = lbls_new[len(lbls_new) - round(len(lbls_new) * 0.3):]
 
 
 # m = 22  # number of rules best for 1
@@ -63,9 +31,9 @@ alpha = 0.001  # learning rate
 batch_size = 700
 
 # cfis = Fuzzyloopa(n_inputs=D, n_rules=m, learning_rate=alpha)
-cfis = CFuzzyloopa(n_inputs=D, n_rules=m, n_output=1, learning_rate=alpha)
+cfis = CFuzzyloopa(n_inputs=D, n_rules=m, n_output=T, learning_rate=alpha)
 # Training
-num_epochs = 10000
+num_epochs = 40000
 # Initialize session to make computations on the Tensorflow graph
 with tf.Session() as sess:
     sess.run(cfis.init_variables)
@@ -88,21 +56,20 @@ with tf.Session() as sess:
             print('mape: {}\nmad: {}\nsmape: {}\n'.format(mape, mad, smape))
             # ctrn_loss, ctrn_pred, cai_, cci_ = cfis.train(sess, trnData_new, trnLbls_new)
         if epoch == num_epochs - 1:
-            # cfis.plot_rules(sess, 2)
-            # print(ai_)
-            # print(ci_)
-            # print(y)
             json_dict = {}
             json_dict['ai'] = ai_.tolist()
             json_dict['ci'] = ci_.tolist()
             json_dict['y'] = y_.tolist()
-            with open('Weights/Algn_22_1.txt', 'w', encoding='utf-8') as f:
+            with open('Weights/Zuerich_22_1_15.txt', 'w', encoding='utf-8') as f:
                 f.write(json.dumps(json_dict))
             val_pred, val_loss = cfis.make_prediction(sess, chkData_new, chkLbls_new)
             val_pred_ex, val_loss_ex = cfis.make_prediction(sess, chkData_new[0:3], chkLbls_new[0:3])
-            # pred = np.vstack((np.expand_dims(trn_pred, 1), np.expand_dims(val_pred, 1)))
             print(val_loss)
-            plt.plot([x for x in range(0, lbls_new.size)], lbls_new, color='blue')
-            plt.plot([x for x in range(lbls_new.size - round(len(lbls_new) * 0.3), lbls_new.size)], val_pred, color='yellow')
+            # res = np.reshape(trn_pred,[1,-1])
+            # res1 = np.reshape(trnData_new, [1, -1])
+            # plt.plot(list(res[0]))
+            # plt.plot(list(res1[0]))
+            plt.plot([x for x in range(0, len(lbls_new))], lbls_new, color='blue')
+            plt.plot([x for x in range(len(lbls_new) - round(len(lbls_new) * 0.3), len(lbls_new))], val_pred, color='yellow')
             plt.plot([x for x in range(0,lbls_new.size - round(len(lbls_new) * 0.3))], trn_pred, color='green')
     plt.show()
