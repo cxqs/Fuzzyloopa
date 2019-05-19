4351d5d50c80ad22934de741f3050c41e8e00431
A6a7H <dikrivenkov@edu.hse.ru>
2019-05-19 14:20:01 +0300

    finalv5

--- Model/Models.py
+++ Model/Models.py
@@ -1,9 +1,11 @@
-import tensorflow as tf
-from sklearn.metrics import mean_absolute_error
-import numpy as np
-import math
-import matplotlib.pyplot as plt
-import json
+# import tensorflow as tf
+# from sklearn.metrics import mean_absolute_error
+# import numpy as np
+# import math
+# import matplotlib.pyplot as plt
+# import json
+
+from Model import *
 
 class CFuzzyloopa():
 
---
+++ Model/__init__.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from sklearn.metrics import mean_absolute_error
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import json
